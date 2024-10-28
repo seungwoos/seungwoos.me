492ba783bd877e9331f160ec715a622d078bb56e
seungwoos <tmddngood@gmail.com>
2024-10-28 23:36:30 +0900

    fix: fix the container layout

--- components/Container.tsx
+++ components/Container.tsx
@@ -5,10 +5,16 @@ import Header from "./Header";
 
 export default function Container({ children }: { children: React.ReactNode }) {
   return (
-    <div className="max-w-xl min-h-screen flex flex-col mx-auto py-10 text-slate-900 dark:text-slate-50 bg-white dark:bg-black">
-      <Header />
-      <main className="flex-grow py-10">{children}</main>
-      <Footer />
+    <div className="min-h-screen flex flex-col mx-auto py-10 text-slate-900 dark:text-slate-50 bg-white dark:bg-black">
+      <div className="max-w-xl mx-auto w-full">
+        <Header />
+      </div>
+      <div className="max-w-xl mx-auto">
+        <main className="flex-grow py-10">{children}</main>
+      </div>
+      <div className="max-w-xl mx-auto w-full">
+        <Footer />
+      </div>
     </div>
   );
 }
