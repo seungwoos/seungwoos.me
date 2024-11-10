44bc040804ddac2bac7f71d44af973efa3a8d19e
seungwoos <tmddngood@gmail.com>
2024-11-10 17:07:33 +0900

    feat: remove unnecessary attributes

--- components/Container.tsx
+++ components/Container.tsx
@@ -5,10 +5,8 @@ import Header from "./Header";
 export default function Container({ children }: { children: React.ReactNode }) {
   return (
     <div className="min-h-window antialiased flex flex-col">
-      <div className="fixed top-0 w-full bg-white/90 dark:bg-black/90 z-10">
-        <Header />
-      </div>
-      <main className="max-w-2xl mx-auto pt-24 flex-grow">{children}</main>
+      <Header />
+      <main className="mx-auto pt-20 flex-grow">{children}</main>
       <Footer />
     </div>
   );
