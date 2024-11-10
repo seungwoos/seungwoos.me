eb1bdff393cd2b54432875d8b7c0d872e7e51526
seungwoos <tmddngood@gmail.com>
2024-11-10 17:07:08 +0900

    feat: update header with material tailwind

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,19 +1,41 @@
 "use client";
+
 import Link from "next/link";
-import DarkModeToggle from "./DarkModeToggle";
+import ThemeToggleButton from "./ThemeToggleButton";
+import { Navbar, Typography } from "@material-tailwind/react";
+
+const NAV_ITEMS = [{ label: "about", href: "/about" }];
 
 export default function Header() {
   return (
-    <nav className="flex items-center justify-between h-16 max-w-2xl mx-auto">
-      <Link href="/">@seungwoos</Link>
-      <ul className="flex items-center space-x-6">
-        <li>
-          <Link href="/about">about</Link>
-        </li>
-        <li>
-          <DarkModeToggle />
-        </li>
-      </ul>
-    </nav>
+    <div className="overflow-scroll">
+      <Navbar
+        className="sticky rounded-none flex items-center justify-between max-w-screen-md mx-auto"
+        shadow={false}
+        color="transparent"
+      >
+        <ul>
+          <Typography className="cursor-pointer font-medium text-black dark:text-white">
+            <Link href="/">@seungwoos</Link>
+          </Typography>
+        </ul>
+        <ul className="flex items-center gap-6">
+          {NAV_ITEMS.map(({ label, href }) => (
+            <li key={href}>
+              <Typography
+                as={Link}
+                href={href}
+                className="font-medium transition-all duration-300 ease-in-out hover:text-gray-300 dark:hover:texts-gray-600 cursor-pointer text-black dark:text-white"
+              >
+                {label}
+              </Typography>
+            </li>
+          ))}
+          <li>
+            <ThemeToggleButton />
+          </li>
+        </ul>
+      </Navbar>
+    </div>
   );
 }
