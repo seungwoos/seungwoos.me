88835799d1b202af8067a458be728276a4cc5d25
seungwoos <tmddngood@gmail.com>
2024-11-10 19:12:14 +0900

    fix: make header sticky

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,45 +8,43 @@ const NAV_ITEMS = [{ label: "about", href: "/about" }];
 
 export default function Header() {
   return (
-    <div className="overflow-scroll">
-      <Navbar
-        className="sticky rounded-none flex items-center justify-between max-w-screen-md mx-auto"
-        shadow={false}
-        color="transparent"
-        placeholder={undefined}
-        onPointerEnterCapture={undefined}
-        onPointerLeaveCapture={undefined}
-      >
-        <ul>
-          <Typography
-            className="cursor-pointer font-medium text-black dark:text-white"
-            placeholder={undefined}
-            onPointerEnterCapture={undefined}
-            onPointerLeaveCapture={undefined}
-          >
-            <Link href="/">@seungwoos</Link>
-          </Typography>
-        </ul>
-        <ul className="flex items-center gap-6">
-          {NAV_ITEMS.map(({ label, href }) => (
-            <li key={href}>
-              <Typography
-                as={Link}
-                href={href}
-                className="font-medium transition-all duration-300 ease-in-out hover:text-gray-300 dark:hover:texts-gray-600 cursor-pointer text-black dark:text-white"
-                placeholder={undefined}
-                onPointerEnterCapture={undefined}
-                onPointerLeaveCapture={undefined}
-              >
-                {label}
-              </Typography>
-            </li>
-          ))}
-          <li>
-            <ThemeToggleButton />
+    <Navbar
+      className="sticky top-0 z-10 h-max px-4 py-2 lg:px-8 lg:py-4 flex items-center justify-between max-w-screen-md mx-auto rounded-none bg-white/90 dark:bg-black/90"
+      shadow={false}
+      color="transparent"
+      placeholder={undefined}
+      onPointerEnterCapture={undefined}
+      onPointerLeaveCapture={undefined}
+    >
+      <ul>
+        <Typography
+          className="cursor-pointer font-medium text-black dark:text-white"
+          placeholder={undefined}
+          onPointerEnterCapture={undefined}
+          onPointerLeaveCapture={undefined}
+        >
+          <Link href="/">@seungwoos</Link>
+        </Typography>
+      </ul>
+      <ul className="flex items-center gap-6">
+        {NAV_ITEMS.map(({ label, href }) => (
+          <li key={href}>
+            <Typography
+              as={Link}
+              href={href}
+              className="font-medium transition-all duration-300 ease-in-out hover:text-gray-300 dark:hover:texts-gray-600 cursor-pointer text-black dark:text-white"
+              placeholder={undefined}
+              onPointerEnterCapture={undefined}
+              onPointerLeaveCapture={undefined}
+            >
+              {label}
+            </Typography>
           </li>
-        </ul>
-      </Navbar>
-    </div>
+        ))}
+        <li>
+          <ThemeToggleButton />
+        </li>
+      </ul>
+    </Navbar>
   );
 }
