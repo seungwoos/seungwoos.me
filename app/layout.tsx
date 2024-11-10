bc5034ed7e4d2815cfc863f377edab9c44881dfe
seungwoos <tmddngood@gmail.com>
2024-11-10 17:05:11 +0900

    feat: add material tailwind and remove unnecessary attributes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,8 @@
 import "./globals.css";
 import { ThemeProvider } from "providers/ThemeProvider";
-import { Space_Grotesk } from "next/font/google";
 import Container from "components/Container";
 import { Analytics } from "@vercel/analytics/react";
 import ReadingProgressBar from "components/ReadingProgressBar";
-
-const inter = Space_Grotesk({ subsets: ["latin"], display: "swap" });
 
 export default function RootLayout({
   children,
@@ -14,10 +11,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body
-        className={`bg-white dark:bg-black text-slate-900 dark:text-slate-50 ${inter.className}`}
-      >
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      <body className="bg-white dark:bg-black">
+        <ThemeProvider>
           <ReadingProgressBar />
           <Container>{children}</Container>
           <Analytics />
