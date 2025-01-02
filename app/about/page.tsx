04d0357c9b718371f4a277c214cd64d9af1a3069
seungwoos <tmddngood@gmail.com>
2025-01-02 15:44:33 +0900

    fix: remove unnecessary layout

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,11 +4,5 @@ import ClientNotionRenderer from "components/ClientNotionRenderer";
 export default async function About() {
   const recordMap = await getAboutPage();
 
-  return (
-    <div className="relative flex">
-      <div className="prose dark:prose-invert prose-p:m-0 max-w-screen-md mx-4 md:mx-8 lg:mx-0">
-        <ClientNotionRenderer recordMap={recordMap} />
-      </div>
-    </div>
-  );
+  return <ClientNotionRenderer recordMap={recordMap} />;
 }
