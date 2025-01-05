aec012c9be0e1d4cea6382ca5f7e7dafda594b15
seungwoos <tmddngood@gmail.com>
2025-01-05 21:47:20 +0900

    feat: add blog post page

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,3 +1,14 @@
-export default async function Post() {
-  return <main>The post content should go here</main>;
+import ClientNotionRenderer from "components/ClientNotionRenderer";
+import { getNotionPage, getPostBySlug } from "lib/notion";
+
+export default async function Post({ params }) {
+  const { slug } = await params;
+  const post = await getPostBySlug(slug);
+  const recordMap = await getNotionPage(post.id);
+
+  return (
+    <article>
+      <ClientNotionRenderer recordMap={recordMap} />
+    </article>
+  );
 }
