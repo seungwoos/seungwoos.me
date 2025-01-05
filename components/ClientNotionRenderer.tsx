020695aa09abe7b0c61dc7f7f8e47cdeb0f1fe50
seungwoos <tmddngood@gmail.com>
2025-01-05 21:44:11 +0900

    fix: update NotionRenderer

--- components/ClientNotionRenderer.tsx
+++ components/ClientNotionRenderer.tsx
@@ -3,12 +3,16 @@
 import Image from "next/image";
 import { NotionRenderer } from "react-notion-x";
 import { Code } from "react-notion-x/build/third-party/code";
-import { Collection } from "react-notion-x/build/third-party/collection";
 import { ExtendedRecordMap } from "notion-types";
-import "react-notion-x/src/styles.css";
 import { useTheme } from "next-themes";
 
-export default function ClientNotionRenderer({ recordMap }) {
+interface NotionRecordMapProps {
+  recordMap: ExtendedRecordMap;
+}
+
+export default function ClientNotionRenderer({
+  recordMap,
+}: NotionRecordMapProps) {
   const { theme } = useTheme();
 
   return (
@@ -17,7 +21,6 @@ export default function ClientNotionRenderer({ recordMap }) {
       components={{
         nextImage: Image,
         Code: Code,
-        Collection: Collection,
       }}
       fullPage={false}
       darkMode={theme === "dark"}
